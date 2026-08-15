9b398fefbbdc55d5304b1b7c87e9d91f9fb8d151
Tim Yau <yaurfu@gmail.com>
2026-08-15 16:41:23 +0800

    Add script to merge pdfs

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # PDF Tools
 
 A collection of PDF scripts.
+
+## Commands
+
+Merge pdfs
+
+```
+pdfmerge --files file1 file2 ... --out merged.pdf
+```
--- pdfmerge.py
+++ pdfmerge.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 import argparse
-from typing import TypedDict
+from pypdf import PdfWriter, PdfReader
+from dataclasses import dataclass
+from pathlib import Path
 
 
-class Args(TypedDict):
+@dataclass
+class Args:
     files: list[str]
     out: str
 
@@ -16,13 +19,31 @@ def parse_args() -> Args:
 
     args = parser.parse_args()
 
-    return {
-        'files': args.files,
-        'out': args.out
-    }
+    return Args(files=args.files, out=args.out)
+
+
+def merge_pdfs(args: Args):
+    writer = PdfWriter()
+    page_count = 0
+    pdf_files = [Path(file) for file in args.files]
+
+    for pdf in pdf_files:
+        reader = PdfReader(str(pdf))
+
+        # add pages
+        for page in reader.pages:
+            writer.add_page(page)
+
+        # add bookmark
+        writer.add_outline_item(pdf.stem, page_count)
+
+        page_count += len(reader.pages)
+
+    with open(args.out, "wb") as f:
+        writer.write(f)
 
 
 if __name__ == "__main__":
     args = parse_args()
 
-    print(args)
+    merge_pdfs(args)
